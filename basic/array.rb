79afdc5dd728334cd70bb4e0d21638087e0a4e3f
dung ta <dung ta>
2016-09-12 05:29:21 +0700

    Remove basic folder

--- basic/array.rb
+++
@@ -1,4 +0,0 @@
-arr = ["string", 1, 1.02, "string 2"]
-arr.each do |item|
-  puts item
-end
--- basic/hash.rb
+++
@@ -1,8 +0,0 @@
-=begin
-  This is multi comment
-  Like /* */
-=end
-hash = {"20132524" => "Nguyen Dinh Manh", "20132605" => "Nguyen Van Minh"}
-hash.each do |key, value|
-  print key, " is ", value, "\n"
-end
